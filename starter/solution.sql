f45d6cd605c34dbebba90d083b2a6963ea02e9d4
sumanbaskar <suman2862008@gmail.com>
2026-09-24 13:37:53 +0530

    RDBMS PROGRAM20 1U25IT107

--- starter/solution.sql
+++ starter/solution.sql
@@ -1,17 +1,15 @@
--- 1. Create the trigger function
 CREATE OR REPLACE FUNCTION log_employee_insert()
 RETURNS TRIGGER AS $$
 BEGIN
-    -- % is replaced dynamically by the inserted employee's name
     RAISE NOTICE 'Employee inserted: %', NEW.emp_name;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
--- 2. Create the AFTER INSERT trigger
-DROP TRIGGER IF EXISTS trg_after_employee_insert ON Employee;
+-- 3. Drop existing trigger if present and create the AFTER INSERT trigger
+DROP TRIGGER IF EXISTS trg_employee_after_insert ON Employee;
 
-CREATE TRIGGER trg_after_employee_insert
+CREATE TRIGGER trg_employee_after_insert
 AFTER INSERT ON Employee
 FOR EACH ROW
 EXECUTE FUNCTION log_employee_insert();
